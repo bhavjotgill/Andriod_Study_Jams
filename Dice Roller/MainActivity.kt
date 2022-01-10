5c79ceaaf00fb0e1138916412460c2b15aa88d62
bhavjotgill <75017284+bhavjotgill@users.noreply.github.com>
2022-01-10 22:00:40 +0530

    Update MainActivity.kt

--- Dice Roller/MainActivity.kt
+++ Dice Roller/MainActivity.kt
@@ -1,127 +1,36 @@
-package com.example.calculator
+package com.example.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
-    lateinit var inputbox:TextView
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_main)
 
-        inputbox=findViewById(R.id.inputbox)
+        val rollButton: Button = findViewById(R.id.button)
+        val textview:TextView = findViewById(R.id.textView)
+        val imageview: ImageView = findViewById(R.id.imageView)
 
-    }
+        rollButton.setOnClickListener{
 
-    var dot:Boolean=false
-    var newop:Boolean=true
-
-    var operator:String = "X"
-    var oldNum:String=""
-
-    fun getNumbers(view: View){
-
-        if(newop){
-            inputbox.text=""
+            var c:Int = (1..6).random()
+            textview.text = c.toString()
+            when(c){
+                1->imageview.setImageResource(R.drawable.dice_1)
+                2->imageview.setImageResource(R.drawable.dice_2)
+                3->imageview.setImageResource(R.drawable.dice_3)
+                4->imageview.setImageResource(R.drawable.dice_4)
+                5->imageview.setImageResource(R.drawable.dice_5)
+                6->imageview.setImageResource(R.drawable.dice_6)
+                else->imageview.setImageResource(R.drawable.dice_6)
+            }
+            Toast.makeText(this, "Dice Rolled", Toast.LENGTH_SHORT).show()
         }
-        newop=false
 
-        var getText:String = inputbox.text.toString()
-
-        val selectedBtn = view as Button
-        when(selectedBtn.id){
-            R.id.bt0->{
-                getText+="0"
-            }
-            R.id.bt1->{
-                getText+="1"
-            }
-            R.id.bt2->{
-                getText+="2"
-            }
-            R.id.bt3->{
-                getText+="3"
-            }
-            R.id.bt4->{
-                getText+="4"
-            }
-            R.id.bt5->{
-                getText+="5"
-            }
-            R.id.bt6->{
-                getText+="6"
-            }
-            R.id.bt7->{
-                getText+="7"
-            }
-            R.id.bt8->{
-                getText+="8"
-            }
-            R.id.bt9->{
-                getText+="9"
-            }
-            R.id.btdot->{
-                if(!dot){
-                    getText+="."
-                }
-                dot=true
-            }
-        }
-        inputbox.text=getText
     }
-
-    fun getOperator(view:View){
-        val selectedOp = view as Button
-        when(selectedOp.id){
-            R.id.btadd->
-            {
-                operator="+"
-            }
-            R.id.btsub->{
-                operator="-"
-            }
-            R.id.btmultiply->{
-                operator="x"
-            }
-            R.id.btdivide->{
-                operator="/"
-            }
-        }
-        oldNum=inputbox.text.toString()
-        dot=false
-        newop=true
-    }
-
-    fun equaloperator(view:View){
-        val newNum = inputbox.text.toString()
-        var result:Double?=null
-        when(operator){
-            "x"->{
-                result=oldNum.toDouble() * newNum.toDouble()
-            }
-            "+"->{
-                result=oldNum.toDouble() + newNum.toDouble()
-            }
-            "-"->{
-                result=oldNum.toDouble() - newNum.toDouble()
-            }
-            "/"->{
-                result=oldNum.toDouble() / newNum.toDouble()
-            }
-        }
-        inputbox.text=result.toString()
-    }
-
-    fun cleanInput(view:View){
-        inputbox.text=""
-        newop=true
-        dot=false
-    }
-
-
 }
